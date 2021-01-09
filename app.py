61322b841ca6c5206d7ae45cdf7b0d852459af63
sree <sreelu.ph@gmail.com>
2021-01-09 01:16:55 -0700

    modified

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from flask_cors import CORS
 app= Flask(__name__)
 CORS(app)
 
-@app.route('/api/blogpost',methods=['GET','POST','PATCH','DELETE'])
+@app.route('/api/post',methods=['GET','POST','PATCH','DELETE'])
 def blogs():
     if request.method=='GET':
         conn=None
